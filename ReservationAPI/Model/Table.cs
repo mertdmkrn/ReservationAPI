f8e52b1ec65f337709dde767c07656d9451caded
mertdmkrn37 <mertdmkrn37@gmail.com>
2023-11-10 01:56:18 +0300

    Table tablosunda json' da gözükmemesi gereken veriler ignore edildi.

--- ReservationAPI/Model/Table.cs
+++ ReservationAPI/Model/Table.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace ReservationAPI.Model
 {
@@ -17,7 +18,9 @@ namespace ReservationAPI.Model
 
         [MaxLength(6)]
         public string Number { get; set; }
-        public int Capacity { get; set; }      
+        public int Capacity { get; set; }
+
+        [JsonIgnore]
         public virtual IEnumerable<Reservation> Reservations { get; set; }      
     }
 }
